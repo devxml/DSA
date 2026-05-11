a30737560c319ba00c0b6f89846c2d9d636bfe72
devxml <devverma2031@gmail.com>
2026-05-11 09:28:00 +0530

    Solution Added - LeetCode to GitHub

---
+++ Binary_Search/Count elements less than or equal to k in a sorted rotated array/count-elements-less-than-or-equal-to-k-in-a-sorted-rotated-array.cpp
@@ -0,0 +1,43 @@
+class Solution {
+    public:
+    int countLessEqual(vector<int>& arr, int x) {
+        int n = arr.size();
+        
+        int low = 0, high = n - 1;
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] > arr[high])
+                low = mid + 1;
+            else
+                high = mid;
+        }
+        int pivot = low;
+
+        auto lastIndex = [&](int l, int r) {
+            int ans = -1;
+            while (l <= r) {
+                int mid = (l + r) / 2;
+                if (arr[mid] <= x) {
+                    ans = mid;
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+            return ans;
+        };
+
+        int count = 0;
+
+        // Search left part
+        int idx1 = lastIndex(0, pivot - 1);
+        if (idx1 != -1) count += (idx1 + 1);
+
+        // Search right part
+        int idx2 = lastIndex(pivot, n - 1);
+        if (idx2 != -1) count += (idx2 - pivot + 1);
+
+        return count;
+        
+    }
+};
